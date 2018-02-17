983f19c20c52474e811ad242d271db3c4b147509
Kazuki Saito <unic.almj@yahoo.com>
2018-02-17 12:42:53 +0000

    blogs contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,15 @@
 class Contact < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, allow_blank: true }
+    # validates :email, presence: true
     validates :name, presence: true
     validates :content, presence: true, length: { maximum: 140 }
+    
+    
+    
+    # with_options unless: -> { :name? || :content? } do 
+    #     validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    # end
 end
+
+
